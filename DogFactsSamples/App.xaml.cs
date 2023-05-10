bb97965349978cf6bc10a54d794c781fa0f3096d
Jeff Dziadulewicz <jeffdziad@gmail.com>
2023-05-10 15:22:58 -0500

    + Fixed duplicate database entries. Forgot ClearAllAsync() before adding data.

--- DogFactsSamples/App.xaml.cs
+++ DogFactsSamples/App.xaml.cs
@@ -45,13 +45,14 @@ namespace DogFactsSamples
 
         static void prefillDatabase()
         {
+            database.ClearAllAsync();
             List<JeffFactData> all = new List<JeffFactData>();
             all.Add(new JeffFactData() { TheFact = "Jeff's favorite band is The Beatles.", ShortFact = "Favorite Band", ImageSource = "thebeatles.jpg" });
             all.Add(new JeffFactData() { TheFact = "Jeff's favorite aircraft is the B-1B Lancer.", ShortFact = "Favorite Aircraft", ImageSource = "b1blancer.jpg" });
             all.Add(new JeffFactData() { TheFact = "Jeff uses Fusion360 to create 3D models and prints them on the ender 3 pro.", ShortFact = "3D Modeling/Printing", ImageSource = "ender3pro.jpg" });
             all.Add(new JeffFactData() { TheFact = "Jeff started learning to play the Guitar when he was 10 years old.", ShortFact = "Learning Guitar", ImageSource = "guitar.jpg" });
             all.Add(new JeffFactData() { TheFact = "Jeff shares a birthday with Payton Manning.", ShortFact = "Shares Birthday", ImageSource = "paytonmanning.jpg" });
-            Database.InsertList(all);
+            database.InsertList(all);
         }
     }
 }
